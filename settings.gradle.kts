6946f2e2c19b1b924ef64937d329906a5dcdede5
Tim te Beek <tim@moderne.io>
2024-12-24 14:13:35 +0100

    Remove pluginManagement and unused variables in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,3 @@
-pluginManagement {
-    repositories {
-        mavenLocal()
-        gradlePluginPortal()
-    }
-}
-
 rootProject.name = "rewrite-cobol"
 
-
-
 include("cobol-cli")
-
-val isCiServer = System.getenv("CI")?.equals("true") ?: false
-
-
