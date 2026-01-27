714b94f93f2e0db481fed0937eba2614c53c1191
Benjamin Muschko <benjamin.muschko@gmail.com>
2026-01-27 15:12:30 -0700

    Remove unnecessary cast

--- src/main/java/org/openrewrite/cobol/search/FindWord.java
+++ src/main/java/org/openrewrite/cobol/search/FindWord.java
@@ -69,7 +69,7 @@ public class FindWord extends Recipe {
                 if (it instanceof CobolPreprocessor.CopyStatement && !hasCopyStatement.get()) {
                     hasCopyStatement.set(true);
                 }
-                return (CobolPreprocessor) preprocessorSearch.visit(it, ctx);
+                return preprocessorSearch.visit(it, ctx);
             }));
 
             if (hasCopyStatement.get() || w.getMarkers().findFirst(CopiedWord.class).isPresent()) {
